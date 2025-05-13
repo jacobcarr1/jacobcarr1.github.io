8e6e5f03ced460fd5c61a53fbe3011af6988089e
jacobcarr1 <carrjr23@juniata.edu>
2025-05-13 17:48:54 -0400

    Update finalproject.js

---
+++ js/finalproject.js
@@ -0,0 +1,19 @@
+checkbox[0].onclick = function(){
+    alert("Aliens Exist was clicked!")
+}
+
+checkbox[1].onclick = function(){
+    alert("What's My Age Again was clicked!")
+}
+
+checkbox[2].onclick = function(){
+    alert("Dumpweed was clicked!")
+}
+
+checkbox[3].onclick = function(){
+    alert("Adam's Song was clicked!")
+}
+
+checkbox[4].onclick = function(){
+    alert("All The Small Things was clicked!")
+}
