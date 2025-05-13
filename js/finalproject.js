9be88c07756df0ee88219c1e0cddd04b489c67bb
jacobcarr1 <carrjr23@juniata.edu>
2025-05-13 17:54:25 -0400

    Update finalproject.js

--- js/finalproject.js
+++ js/finalproject.js
@@ -1,3 +1,12 @@
+function clearFields(reset) {
+    document.getElementById("clear").reset();
+}
+const Aliens Exist = document.querySelector("#Aliens Exist");
+const Whats My Age Again = document.querySelector("#What's My Age Again");
+const Dumpweed = document.querySelector("#Dumpweed");
+const Adams Song = document.querySelector("#Adam's Song");
+const All The Small Things = document.querySelectorAll("#All The Small Things");
+
 checkbox[0].onclick = function(){
     alert("Aliens Exist was clicked!")
 }
