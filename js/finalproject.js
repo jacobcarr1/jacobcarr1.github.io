443669d644a67f1c1660a7f04cdb8441b07beb4b
jacobcarr1 <carrjr23@juniata.edu>
2025-05-13 20:33:53 -0400

    Update finalproject.js

---
+++ js/finalproject.js
@@ -0,0 +1,14 @@
+const checkboxes = document.querySelectorAll(".song-checkbox");
+
+const messages = [
+  "Aliens Exist was clicked!",
+  "What's My Age Again was clicked!",
+  "Dumpweed was clicked!",
+  "Adam's Song was clicked!",
+  "All The Small Things was clicked!"
+];
+
+// Attach click event to each checkbox
+checkboxes.forEach((box, index) => {
+  box.onclick = () => alert(messages[index]);
+});
