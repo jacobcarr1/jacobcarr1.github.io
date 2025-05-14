5c24f22b292eb60c762e3264bcacf9360f1a1149
jacobcarr1 <carrjr23@juniata.edu>
2025-05-13 22:39:52 -0400

    Update projectfinal.js

--- js/projectfinal.js
+++ js/projectfinal.js
@@ -32,7 +32,7 @@ function checkFavorite() {
         message.innerHTML = "Please select a member!";
     }
 }
- // Keep the message visible for 5 seconds (5000 milliseconds)
+ // Keep the message visible for 5 seconds
     setTimeout(function() {
         message.innerHTML = ""; // Clear the message after 5 seconds
     }, 5000);  // 5000 ms = 5 seconds
