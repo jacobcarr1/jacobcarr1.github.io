cbceae88b574781bb2da3ff7929c77f8803f1e4c
jacobcarr1 <carrjr23@juniata.edu>
2025-05-13 22:50:40 -0400

    Update projectfinal.js

--- js/projectfinal.js
+++ js/projectfinal.js
@@ -20,7 +20,7 @@ function showEmail() {
 
 function checkFavorite() {
     const fav = document.getElementById("favMember").value;
-    const message = document.getElementById("memberMessage");
+    const message = document.getElementById("memberMessage").value;
 
     if (fav === "Tom") {
         message.innerHTML = "Great choice!";
